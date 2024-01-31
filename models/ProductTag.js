212e7442124d1fdefbf01f730bdf5b34e22f7ff3
Shane <s10skeleton@gmail.com>
2024-01-30 20:40:44 -0700

    Further Route and Model edits

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,22 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product', // references the 'product' table
+        key: 'id', // references the 'id' column in the 'product' table
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag', // references the 'tag' table
+        key: 'id', // references the 'id' column in the 'tag' table
+      },
+    },
   },
   {
     sequelize,
